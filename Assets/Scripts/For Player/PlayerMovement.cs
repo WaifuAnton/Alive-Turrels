f4348346e356ab56ed8d84f14c0a487bf0d845c6
Anton Makarenko <makarenkoanton2013@gmail.com>
2018-07-18 17:17:02 +0300

    player & sample turrel

---
+++ Assets/Scripts/For Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 6;
+    CharacterController character;
+	// Use this for initialization
+	void Start () {
+        character = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        moveX *= speed;
+        moveZ *= speed;
+        Vector3 movement = new Vector3(moveX, 0, moveZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        character.Move(movement);
+	}
+}
---
+++ Assets/Scripts/For Player/PlayerRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour {
+    public float speedX = 7;
+    public float speedY = 7;
+    public float minAngle = -45;
+    public float maxAngle = 45;
+    public enum rotationAxis { axisX, axisY };
+    public rotationAxis axis;
+    float rotation = 0;
+    float rotY;
+	// Use this for initialization
+	void Start () {
+        rotY = transform.localRotation.y;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float rotationX = Input.GetAxis("Mouse Y") * speedX;
+        float rotationY = Input.GetAxis("Mouse X") * speedY;
+		switch (axis)
+        {
+            case rotationAxis.axisY:
+                transform.Rotate(0, rotationY, 0);
+                break;
+            case rotationAxis.axisX:
+                rotation -= rotationX;
+                rotation = Mathf.Clamp(rotation, minAngle, maxAngle);
+                transform.localRotation = Quaternion.Euler(rotation, rotY, 0);
+                break;
+        }
+	}
+}
