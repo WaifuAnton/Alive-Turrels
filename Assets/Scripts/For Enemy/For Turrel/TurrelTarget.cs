a221a00a9c0c74e78d73e6531f3e6cdf703ea498
Anton Makarenko <makarenkoanton2013@gmail.com>
2018-07-22 18:18:43 +0300

    begun to work on players shooting

---
+++ Assets/Scripts/For Enemy/For Turrel/TurrelTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrelTarget : MonoBehaviour {
+    public float health = 5;
+    float curHealth;
+	// Use this for initialization
+	void Start () {
+        curHealth = health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeHealth(int value)
+    {
+        curHealth += value;
+        if (curHealth > health)
+            curHealth = health;
+        if (curHealth <= 0)
+        {
+            curHealth = 0;
+            //transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 90);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/For Player/PlayerShooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+    public int damage = 2;
+    Camera playerCamera;
+	// Use this for initialization
+	void Start () {
+        playerCamera = transform.GetChild(0).GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(playerCamera.pixelWidth / 2, playerCamera.pixelHeight / 2);
+            Ray ray = playerCamera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                TurrelTarget turrel = hit.transform.GetComponent<TurrelTarget>();
+                if (turrel != null)
+                {
+                    turrel.ChangeHealth(-damage);
+                    Debug.Log(true);
+                }
+            }
+        }
+	}
+
+    void OnGUI() { 
+        GUI.Label(new Rect(playerCamera.pixelWidth / 2 - 3, playerCamera.pixelHeight / 2 - 8, 100, 100), "+");
+    }
+}
